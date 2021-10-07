6034e34351beeb412805f1b0281682d3bc28cccf
vasiliev-alexey <alex_v81@mail.ru>
2021-10-07 18:59:01 +0300

    chang ci-cd

--- packages/hw1/webpack.config.ts
+++ packages/hw1/webpack.config.ts
@@ -52,8 +52,7 @@ const webpackConfig = (
   output: {
     path: path.join(
       __dirname,
-      arg.mode === 'production' ? '../../dist/' : '',
-      'dist'
+      arg.mode === 'production' ? '../../dist/' : 'dist'
     ),
 
     filename: 'index.js',
