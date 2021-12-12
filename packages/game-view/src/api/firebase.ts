b4bafbf142129a16e36e5c4e64ce4383378cae61
vasiliev-alexey <alex_v81@mail.ru>
2021-12-12 16:07:43 +0300

    fix configs for ghp

--- packages/game-view/src/api/firebase.ts
+++ packages/game-view/src/api/firebase.ts
@@ -5,7 +5,7 @@ import 'firebase/firestore';
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  projectId: 'inbound-coast-275214', //process.env.REACT_APP_PROJECT_ID,
+  projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGEBUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
