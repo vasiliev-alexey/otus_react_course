ef081447f623bd907739b92f2f7e03fc3748183c
vasiliev-alexey <alex_v81@mail.ru>
2021-12-19 21:55:33 +0300

    add leader board slice

---
+++ packages/game-view/src/api/db.ts
@@ -0,0 +1,44 @@
+import { db } from './firebase';
+
+const GAMERS = 'gamers';
+
+export const getTopGamerList = async (limitSize = 10): Promise<Gamer[]> => {
+  try {
+    const data = await db
+      .collection(GAMERS)
+      .orderBy('topScore')
+      .limit(limitSize)
+      .get();
+
+    return data.docs.map((d) => {
+      return {
+        userName: d.get('userName'),
+        pictUrl: d.get('pictUrl'),
+        uid: d.get('uid'),
+        topScore: d.get('topScore'),
+      };
+    });
+  } catch (e) {
+    console.error(e); // eslint-disable-line no-console
+  }
+};
+
+export const saveUserResult = async (user: Gamer): Promise<boolean> => {
+  try {
+    const data = await db.collection(GAMERS).doc(user.uid).get();
+    if ((data.exists && data.get('topScore') < user.topScore) || !data.exists) {
+      await db.collection(GAMERS).doc(user.uid).set(user, { merge: true });
+    }
+    return true;
+  } catch (e) {
+    console.error(e); // eslint-disable-line no-console
+    return false;
+  }
+};
+
+export interface Gamer {
+  uid: string;
+  pictUrl: string;
+  userName: string;
+  topScore: number;
+}
--- packages/game-view/src/store/store.ts
+++ packages/game-view/src/store/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import authReducer from './authSlice';
+import leaderBoardReducer from './leaderBoardSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    leaderBoard: leaderBoardReducer,
   },
 });
 
