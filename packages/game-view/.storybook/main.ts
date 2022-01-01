d414a63ac1b55d902633a5f610c28af9954a8119
vasiliev-alexey <alex_v81@mail.ru>
2022-01-01 21:30:29 +0300

    add babel plugin 2

--- packages/game-view/.storybook/main.ts
+++ packages/game-view/.storybook/main.ts
@@ -1,3 +1,10 @@
+import customConfig from '../webpack.config';
+
+const custCfg = customConfig(
+  { production: false, development: true },
+  { mode: 'production' }
+);
+
 module.exports = {
   stories: [
     '../src/components/game/GameView.stories.tsx', // default page
@@ -11,6 +18,19 @@ module.exports = {
   core: {
     builder: 'webpack5',
   },
+
+  webpackFinal: async (config) => {
+    let cgf = {
+      ...config,
+      plugins: [...config.plugins, ...custCfg.plugins],
+      module: {
+        ...config.module,
+        rules: custCfg.module.rules,
+      },
+    };
+    return cgf;
+  },
+
   typescript: {
     check: false,
     checkOptions: {},
